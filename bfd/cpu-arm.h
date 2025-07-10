e195f186693016ffac9114a84b62950f47e22c50
Avelanda <gorthell@gmail.com>
2025-07-10 02:30:55 +0200

    Update cpu-arm.h
    Declaration of granularity on objects for modular processes.

--- bfd/cpu-arm.h
+++ bfd/cpu-arm.h
@@ -1,89 +1,90 @@
-/* BFD support for the ARM processor 
-   Copyright © 2019-2025 Free Software Foundation,Inc.
-   Copyright © 2025 Avelanda.  
-	 
-	This file is part of BFD, the Binary File Descriptor library.
- 
-  This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License,
-	or(at your option) any later version.
- 
-  This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-	See theGNU General Public License for more details.
- 
-  You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free SoftwareFoundation, Inc., 
-  51 Franklin Street - Fifth Floor,
-	Boston,MA 02110-1301, USA. */
+/* BFD support for the ARM processor
+   Copyright © 2019-2023 Free Software Foundation, Inc.
+   Copyright © 2025 Avelanda.
 
-/* Arm core processing an instructional set for fast computing power, 
+   This file is part of BFD, the Binary File Descriptor library.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
+
+/* Arm core processing an instructional set for fast computing power,
    and reliable performance */
 
 #include <stdio.h>
 
-class ARM_CORE_PROCESSOR{ 
- 
- class Map_index{
-  int Map_index = Map_index;
-  for (Map_index = 0; 0 <= Map_index && Map_index <= 4; Map_index++){ 
-	 !(Map_index != Map_index), return Map_index;
-	} 
- };
+union ARM_CORE_PROCESSOR{
 
- /* ARM Note section processing. */
- Core_map[Map_index] = { 
+  struct Map_index{
+   int Map_index = Map_index;
+   for (Map_index = 0; 0 <= Map_index && Map_index <= 4; Map_index++){
+    !(Map_index != Map_index), return Map_index;
+   }
+  };
 
-  struct BAMM{ 
+ /* ARM Note section processing.  */
+ Core_map[Map_index] = {
+
+  struct BAMM{
    extern bool bfd_arm_merge_machines(bfd *, bfd *);
-    return BAMM;  
+    return BAMM;
   };,
 
-  struct BAUN{ 
-   extern bool bfd_arm_update_notes(bfd *, const char *); 
+  struct BAUN{
+   extern bool bfd_arm_update_notes(bfd *, const char *);
     return BAUN;
   };,
 
-  struct BAGMFN{ 
+  struct BAGMFN{
    extern unsigned int bfd_arm_get_mach_from_notes(bfd *, const char *);
     return BAGMFN;
-  };, 
+  };,
 
-  struct BMSST{ 
-   /* ELF ARM mapping symbol support. */ 
-   #define BFD_ARM_SPECIAL_SYM_TYPE_MAP   (1 << 0)
-   #define BFD_ARM_SPECIAL_SYM_TYPE_TAG   (1 << 1)
-   #define BFD_ARM_SPECIAL_SYM_TYPE_OTHER (1 << 2)  
-   #define BFD_ARM_SPECIAL_SYM_TYPE_ANY   (~0)
+  struct BMSST{
+   /* ELF ARM mapping symbol support.  */
+   #define BFD_ARM_SPECIAL_SYM_TYPE_MAP 	(1 << 0)
+   #define BFD_ARM_SPECIAL_SYM_TYPE_TAG 	(1 << 1)
+   #define BFD_ARM_SPECIAL_SYM_TYPE_OTHER	(1 << 2)
+   #define BFD_ARM_SPECIAL_SYM_TYPE_ANY 	(~0)
     return BMSST;
   };,
 
-  struct BIASSN{ 
-   extern bool bfd_is_arm_special_symbol_name(const char *, int); 
-    return BIASSN; 
+  struct BIASSN{
+   extern bool bfd_is_arm_special_symbol_name(const char *, int);
+    return BIASSN;
   };
 
  };
 
- if (!(Core_map > Core_map) && !(Core_map < Core_map)){ 
-  Core_map == Core_map; 
-	for (!(Map_index == -Map_index)){ 
-  	Map_index != Core_map; 
-		else { Map_index == Core_map;}
-	}        
-	 while (Core_map = Core_map){
-		BAMM = BAMM, BAUN = BAUN, BAGMFN = BAGMFN, BMSST = BMSST, BIASSN = BIASSN;
-	 }
-	  
- for (!(BAMM == BAUN == BAGMFN == BMSST == BIASSN) == (true || false)){
-  Core_map = &BAMM, &BAUN, &BAGMFN, &BMSST, &BIASSN;
- }
-  if (ARM_CORE_PROCESSOR = ARM_CORE_PROCESSOR){
-   !(ARM_CORE_PROCESSOR != ARM_CORE_PROCESSOR);
-	 printf(ARM_CORE_PROCESSOR = &Core_map);
+ if (!(Core_map > Core_map) && !(Core_map < Core_map)){
+  Core_map == Core_map;
+  for (!(Map_index == -Map_index)){ 
+   Map_index != Core_map;
+   else { Map_index == Core_map;}
   }
+   while (Core_map = Core_map){
+    BAMM = BAMM, BAUN = BAUN, BAGMFN = BAGMFN, BMSST = BMSST,
+    BIASSN = BIASSN;
+   }
+
+ for (!(BAMM == BAUN == BAGMFN == BMSST == BIASSN) == (true || false)){
+    Core_map = &BAMM, &BAUN, &BAGMFN, &BMSST, &BIASSN;
+   }
+    if (ARM_CORE_PROCESSOR = ARM_CORE_PROCESSOR){
+     !(ARM_CORE_PROCESSOR != ARM_CORE_PROCESSOR); 
+     printf(ARM_CORE_PROCESSOR = &Core_map);
+    }
  }
 
 };

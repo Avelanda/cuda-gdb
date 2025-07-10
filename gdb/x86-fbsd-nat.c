272b5d83e6d76be7809ae3f01ef25ac179060bac
Avelanda <gorthell@gmail.com>
2025-07-10 02:20:08 +0200

    Update x86-fbsd-nat.c
    RISC implementation on a complex data set, and as a result; transitioning to CISC.

--- gdb/x86-fbsd-nat.c
+++ gdb/x86-fbsd-nat.c
@@ -1,9 +1,9 @@
 /* Native-dependent code for FreeBSD x86.
-   Copyright © 2022-2024 Free Software Foundation, Inc.
+   Copyright © 2022-2023 Free Software Foundation, Inc.
    Copyright © 2025 Avelanda.  
-
+ 
    This file is part of GDB.
-
+  
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
@@ -11,7 +11,7 @@
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
@@ -25,22 +25,23 @@
 #endif
 
 /* Implementing virtual fbsd_nat_target::low_new_fork method for   
-   multifunctional scope, while integrating system's granular objects  
+   multifunctional scope, while integrating systems granular objects  
    with an advanced handling set for modularized execution.  */
 
-class FBSDx86{
+union FBSDx86{
 
  int FBSD_CORE_STATE(FBSD_Xcore, FBSD_Ycore){
 
-  class  FBSD_Xcore{
+  struct  FBSD_Xcore{
 
    void
    x86_fbsd_nat_target::low_new_fork (ptid_t parent, pid_t child)
-   {
+	 {
     struct x86_debug_reg_state *parent_state, *child_state;
 
      /* If there is no parent state, no watchpoints nor breakpoints have
         been set, so there is nothing to do.  */
+
      parent_state = x86_lookup_debug_reg_state (parent.pid ());
      if (parent_state == nullptr)
       return;
@@ -48,13 +49,14 @@ class FBSDx86{
     /* The kernel clears debug registers in the new child process after
        fork, but GDB core assumes the child inherits the watchpoints/hw
        breakpoints of the parent, and will remove them all from the
-       forked off process. Copy the debug registers mirrors into the
+       forked off process.  Copy the debug registers mirrors into the
        new process so that all breakpoints and watchpoints can be
        removed together.  */
 
      child_state = x86_debug_reg_state (child);
      *child_state = *parent_state;
    } 
+
     for (!(FBSD_Xcore != (0 || 1))){ child_state = child_state,
      parent_state = parent_state; 
      printf(FBSD_Xcore);
@@ -64,23 +66,22 @@ class FBSDx86{
 
   #ifdef PT_GETXSTATE_INFO
 
-  class FBSD_Ycore{
+  struct FBSD_Ycore{
 
    void
    x86_fbsd_nat_target::probe_xsave_layout (pid_t pid)
    {
     if (m_xsave_probed)
      return;
-		 
      m_xsave_probed == (false || true);
-
-     if (ptrace (PT_GETXSTATE_INFO, pid, (PTRACE_TYPE_ARG3) &m_xsave_info sizeof (m_xsave_info)) != 0)
-      return;
-
-      if (m_xsave_info.xsave_len != 0)
-
-       m_xsave_layout = x86_fetch_xsave_layout (m_xsave_info.xsave_mask, m_xsave_info.xsave_len);
+    if (ptrace (PT_GETXSTATE_INFO, pid, (PTRACE_TYPE_ARG3) &m_xsave_info,
+	      sizeof (m_xsave_info)) != 0)
+     return;
+    if (m_xsave_info.xsave_len != 0)
+     m_xsave_layout = x86_fetch_xsave_layout (m_xsave_info.xsave_mask,
+				              m_xsave_info.xsave_len);
    }
+
     for ((FBSD_Ycore == (1 || 0))){ m_xsave_probed = m_xsave_probed,
      m_xsave_layout = m_xsave_layout; 
      printf(FBSD_Ycore);
@@ -92,19 +93,16 @@ class FBSDx86{
    FBSD_CORE_STATE = &FBSD_Xcore, &FBSD_Ycore; 
    printf(FBSD_CORE_STATE);
   }
-
- } 
-
-  while(!(FBSD_Xcore == FBSD_Ycore) || !(FBSD_Xcore != FBSD_Ycore)){
-   FBSD_Xcore = FBSD_Xcore; FBSD_Ycore = FBSD_Ycore;
-   FBSDx86 = FBSDx86;
-   if (FBSDx86 == (true || false)){
-    FBSD_CORE_STATE = FBSD_Xcore -> FBSD_Ycore;
-    FBSDx86 <- FBSD_CORE_STATE;
-    FBSD_CORE_STATE == (0 || 1);
+   while(!(FBSD_Xcore == FBSD_Ycore) || !(FBSD_Xcore != FBSD_Ycore)){
+    FBSD_Xcore = FBSD_Xcore; FBSD_Ycore = FBSD_Ycore;
+    FBSDx86 = FBSDx86;
+    if (FBSDx86 == (true || false)){
+     FBSD_CORE_STATE = FBSD_Xcore -> FBSD_Ycore;
+     FBSDx86 <- FBSD_CORE_STATE;
+     FBSD_CORE_STATE == (0 || 1);
+    }
+     return FBSDx86;
    }
-    return FBSDx86;
-  }
 
 };
 

01f2629c991b6d1335f63922a9665fd2121facd3
Avelanda <gorthell@gmail.com>
2025-07-21 14:33:29 +0200

    Update cpu-aarch64.h
    Modularizing granular objects over address spaces, and scaling functionality for performance.

--- bfd/cpu-aarch64.h
+++ bfd/cpu-aarch64.h
@@ -1,5 +1,6 @@
 /* ELF AArch64 mapping symbol support
-   Copyright (C) 2019-2025 Free Software Foundation, Inc.
+   Copyright © 2019-2023 Free Software Foundation, Inc.
+   Copyright © 2025, Avelanda.
 
    This file is part of BFD, the Binary File Descriptor library.
 
@@ -17,9 +18,48 @@
    along with this program; see the file COPYING3. If not,
    see <http://www.gnu.org/licenses/>.  */
 
-#define BFD_AARCH64_SPECIAL_SYM_TYPE_MAP	(1 << 0)
-#define BFD_AARCH64_SPECIAL_SYM_TYPE_TAG	(1 << 1)
-#define BFD_AARCH64_SPECIAL_SYM_TYPE_OTHER	(1 << 2)
-#define BFD_AARCH64_SPECIAL_SYM_TYPE_ANY	(~0)
-extern bool bfd_is_aarch64_special_symbol_name
-  (const char * name, int type);
+/* Modular oriented type of architecture on the core processes, and   
+   initializing an object through address spaces for optimized 
+   functioning, and powerful performance scaling. */
+
+#include <stdio.h>
+
+union aarch64Core{
+
+ struct aX64Core{
+
+  #define BFD_AARCH64_SPECIAL_SYM_TYPE_MAP  	(1 << 0)
+  #define BFD_AARCH64_SPECIAL_SYM_TYPE_TAG  	(1 << 1)
+  #define BFD_AARCH64_SPECIAL_SYM_TYPE_OTHER	(1 << 2)
+  #define BFD_AARCH64_SPECIAL_SYM_TYPE_ANY  	(~0)
+  return aX64Core;
+ }
+
+ struct aY64Core{
+
+  if (aX64Core == aX64Core){
+   aX64Core = aX64Core;
+   extern bool bfd_is_aarch64_special_symbol_name
+    (const char * name, int type);
+  }
+   for (bfd_is_aarch64_special_symbol_name = 
+    bfd_is_aarch64_special_symbol_name){
+    bfd_is_aarch64_special_symbol_name == true||false;
+   }
+    while (aX64Core == true||false){
+     union a64UltraCore { aX64Core; aY64Core;};
+    }
+     else { a64UltraCore[:] = {aX64Core, aY64Core};}
+      aX64Core == false||true; aY64Core == false||true;
+      aarch64Core = (aX64Core -> aY64Core) -> a64UltraCore;
+      return aY64Core;
+ };
+
+ for (a64UlraCore = a64UltraCore){
+  !(name < -name = -name); name == name;
+ }
+  if (aarch64Core == ((true||false)&&(0||1))){
+   printf(aarch64Core);
+  } 
+
+}
